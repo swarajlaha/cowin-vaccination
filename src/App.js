cf3eb325a8bb2a31c0bbf0625b06fc97a7f77d0c
swarajlaha <swaraj.srj@gmail.com>
2021-06-01 21:51:50 +0530

    get & render dist id, centers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SearchByDistrict from './components/searchByDistrict';
 
 function App() {
   return (
     <div className="App">
-      
+      <SearchByDistrict />
     </div>
   );
 }
---
+++ src/components/searchByDistrict.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const SearchByDistrict = () => {
+  const [centers, setCenters] = useState([]);
+  const [distId, setDistId] = useState(446);
+  const [districts, setDistricts] = useState([]);
+  const [distIdBtnClick, setDistIdBtnClick] = useState(446);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${distId}&date=05-06-2021`
+      )
+      .then((res) => {
+        setCenters(res.data.centers);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [distIdBtnClick]);
+
+  useEffect(() => {
+    axios
+      .get(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/26`)
+      .then((res) => {
+        console.log(res);
+        setDistricts(res.data.districts);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const btnClickHandler = () => {
+    setDistIdBtnClick(distId);
+  };
+
+  return (
+    <div>
+      <ul>
+        {districts.map((district) => (
+          <li key={district.district_id}>
+            {district.district_name} - {district.district_id}
+          </li>
+        ))}
+      </ul>
+      <input
+        type="number"
+        value={distId}
+        onChange={(e) => setDistId(e.target.value)}
+      />
+      <button type="button" onClick={btnClickHandler}>
+        OK
+      </button>
+      <ul>
+        {centers.map((center) => (
+          <li key={center.center_id}>{center.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchByDistrict;
