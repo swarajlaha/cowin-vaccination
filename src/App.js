44f045233ea30ba95f12e7dd9de500daf8456181
swarajlaha <swaraj.srj@gmail.com>
2021-06-06 21:34:50 +0530

    route path for availablity, stats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
 import ShowDistrictCode from "./components/showDistrictCode";
+import AppNavbar from "./components/appNavbar";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Statistics from './components/statistics';
 
 function App() {
   return (
     <>
-      <ShowDistrictCode />
+      <AppNavbar />
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ShowDistrictCode} />
+        <Route exact path="/statistics" component={Statistics} />
+      </Switch>
+    </BrowserRouter>
     </>
   );
 }
